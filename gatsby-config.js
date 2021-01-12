9bdd095d35449592d3c8de8bc7226be33a3dc9cc
harsimus <michael@harsim.us>
2021-01-11 19:27:18 -0500

    Updated metadata in the Gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 module.exports = {
   siteMetadata: {
     title: "nyctekken.com",
+    author: {
+      name: `NYC TKN Team`,
+      summary: `Designed and developed by @harsimus`,
+    },
+    description: "The TEKKEN community of New York City and the tri-state area."
+    siteUrl: "https://nyctekken.com",
+    social: {
+      twitter: 'nyctekken',
+    },
   },
   plugins: [
     "gatsby-plugin-netlify-cms",
